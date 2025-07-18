17fe1f7f7978ec1d48757e4a5967017b3ba9ec7f
Shruti <shrutidalve1136@gmail.com>
2025-07-18 16:10:23 +0530

    Get Computer Choice

--- game/rock-paper-scissor.js
+++ game/rock-paper-scissor.js
@@ -1,3 +1,15 @@
 function getComputerChoice(){
-    let randomNum = Math.floor(Math.random*3);
+    let randomNum = Math.floor(Math.random()*3);
+    return randomNum;
+}
+
+let CompChoice = getComputerChoice();
+
+if(CompChoice==1){
+    console.log("Rock");
+}
+else if(CompChoice==2){
+    console.log("Paper");
+}else{
+    console.log("Scissors");
 }
