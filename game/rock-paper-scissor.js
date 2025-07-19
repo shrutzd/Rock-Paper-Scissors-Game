d9dd4cdde858b12da19293979820dc708e13b710
Shruti <shrutidalve1136@gmail.com>
2025-07-19 13:03:46 +0530

    Final Commit, Game over

--- game/rock-paper-scissor.js
+++ game/rock-paper-scissor.js
@@ -4,20 +4,6 @@ function getComputerChoice(){
     return randomNum;
 }
 
-let computerChoiceValue = getComputerChoice();
-
-if(computerChoiceValue==0){
-    randomNum = "rock";
-    console.log("Computer choice: ",randomNum);
-}
-else if(computerChoiceValue==1){
-    randomNum = "paper";
-    console.log("Computer choice: ",randomNum);
-}else{
-    randomNum = "scissor";
-    console.log("Computer choice: ",randomNum);
-}
-
 function getHumanChoice(){
     let HumanChoice = prompt("Rock! Paper! Scissor!");
     return HumanChoice;
@@ -27,11 +13,7 @@ let choiceMap = {
     paper: 1,
     scissor:2
 };
-let humanChoice = getHumanChoice();
-console.log("Your choice: ",humanChoice);
 
-humanChoice = humanChoice.toLowerCase();
-let humanChoiceValue = choiceMap[humanChoice];
 
 let HumanScore = 0, ComputerScore =0;
 
@@ -39,13 +21,49 @@ function playRound(human,computer){
     if(human==computer){
         console.log("It's a tie!");
     }else{
-        if(human==1 && computer==3){
+        if(human===0 && computer===2 ||
+            human===1 && computer ===0 ||
+            human ===2 && computer ===1
+        ){
             console.log(`You win!`);
+            HumanScore++;
         }
         else if(human<computer){
             console.log(`You lose!`);
+            ComputerScore++;
         }
     }
 }
 
-playRound(humanChoiceValue,computerChoiceValue);
+function playGame(){
+    for(let round=1; round<=5; round++){
+        let computerChoiceValue = getComputerChoice();
+
+        if(computerChoiceValue==0){
+        console.log("Computer choice: rock");
+    }
+        else if(computerChoiceValue==1){
+        console.log("Computer choice: paper");
+    }else{
+    console.log("Computer choice: scissor");
+    }
+
+    let humanChoice = getHumanChoice();
+    console.log("Your choice: ",humanChoice);
+
+    humanChoice = humanChoice.toLowerCase();
+    let humanChoiceValue = choiceMap[humanChoice];
+
+    playRound(humanChoiceValue,computerChoiceValue);
+    }
+    if(HumanScore>ComputerScore){
+        console.log("You win this round!");
+    }
+    else if(HumanScore===ComputerScore){
+        console.log("This round is a tie!");
+    }else{
+        console.log("You lose this round!");
+    }
+}
+
+playGame();
